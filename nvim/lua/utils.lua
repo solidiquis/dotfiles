6dfd471544c18b12301b348f16137ff5e5a96d66
Benji Nguyen <benjamin.van.nguyen@gmail.com>
2022-02-27 19:17:13 -0800

    some basic utils

--- nvim/lua/utils.lua
+++ nvim/lua/utils.lua
@@ -1,19 +1,63 @@
-local mode = require("consts").modes
+function alias(func, alias, options)
+  local opts = { args = false }
 
-function _G.nt_find()
+  if options then
+    opts = vim.tbl_extend("force", opts, options)
+  end
+  
+  if not opts.args then
+    vim.cmd(string.format("command! %s lua %s()", alias, func))
+    return
+  end
+
+  local tmpl = "command! -nargs=1 %s lua %s(make_arg_tbl(<q-args>))"
+  local stmt = string.format(tmpl, alias, func)
+  vim.cmd(stmt)
+end
+
+function make_arg_tbl(args)
+  local result = {}
+  local i = 1
+  for arg in vim.gsplit(args, " ", false) do
+    result[i] = arg
+    i = i + 1
+  end
+
+  return unpack(result)
+end
+
+function nt_find()
   vim.api.nvim_command("NERDTreeFind")
 end
+alias("nt_find", "F")
 
-function _G.so()
+function source()
   vim.api.nvim_command("source $MYVIMRC")
 end
+alias("source", "So")
 
-function _G.line_up()
-  vim.api.nvim_feedkeys("^dg_k$A", mode.normal, false)
-  vim.api.nvim_feedkeys("pjdd", mode.normal, false)
+function line_up()
+  local seq = vim.api.nvim_replace_termcodes(
+    "^dg_k$A <Esc>pjdd",
+    true,
+    false,
+    true
+  )
+  vim.api.nvim_feedkeys(seq, "n", false)
+end
+alias("line_up", "LU")
+
+function ft()
+  vim.cmd("set filetype?")
 end
 
-function _G.inspect(...)
+function cc_rfp()
+  local file_name = vim.split(vim.api.nvim_buf_get_name(0), vim.fn.getcwd())[2]
+  local cmd = string.format("printf %s | pbcopy", file_name)
+  os.execute(cmd)
+end
+
+function inspect(...)
   local objects = {}
   for i = 1, select('#', ...) do
     local v = select(i, ...)
@@ -23,3 +67,19 @@ function _G.inspect(...)
   print(table.concat(objects, '\n'))
   return ...
 end
+
+-- WIP
+--function _G.test()
+  ----top_mark = vim.api.nvim.get_buf_get_mark(0, "<")
+  ----bot_mark = vim.api.nvim.get_buf_get_mark(0, ">")
+
+  --print(vim.api.nvim_get_mark())
+--end
+
+-- example of cmd with args:
+-- :Foo 1 2
+function test(a, b)
+  print(a, b)
+end
+alias("test", "Foo", { args = true })
+
