44912af13dec7959d91e399bfaf6e3dd94ff1be6
Benji Nguyen <benjamin.van.nguyen@gmail.com>
2022-02-28 12:03:19 -0800

    no italics for colorscheme

--- nvim/lua/setup/tokyonight.lua
+++ nvim/lua/setup/tokyonight.lua
@@ -1,5 +1,9 @@
 vim.g.tokyonight_style = "night"
-vim.g.tokyonight_italic_functions = true
+vim.g.tokyonight_italic_functions = false
+vim.g.tokyonight_italic_comments = false
+vim.g.tokyonight_italic_keywords = false
+vim.g.tokyonight_italic_functions = false
+vim.g.tokyonight_italic_variables = false
 
 -- Load the colorscheme
 vim.cmd[[colorscheme tokyonight]]
