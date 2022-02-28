79fe463f10e070b44af027e6967c1a8f84053f18
Benji Nguyen <benjamin.van.nguyen@gmail.com>
2022-02-28 15:33:26 -0800

    themes

--- nvim/lua/setup/tokyonight.lua
+++ nvim/lua/setup/tokyonight.lua
@@ -1,4 +1,10 @@
-vim.g.tokyonight_style = "night"
+local theme_styles = {
+  night = "night",
+  day = "day",
+  storm = "storm"
+}
+
+vim.g.tokyonight_style = theme_styles.night
 vim.g.tokyonight_italic_functions = false
 vim.g.tokyonight_italic_comments = false
 vim.g.tokyonight_italic_keywords = false
