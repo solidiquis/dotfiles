5aef71b31685635b9bb0ac278034d6dd866d3ab6
Benji Nguyen <benjamin.van.nguyen@gmail.com>
2022-07-28 09:35:22 -0700

    close file tree tab when toggling nvim tree

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -17,3 +17,12 @@ vim.api.nvim_create_autocmd("BufWritePost", {
     vim.cmd [[ source $MYVIMRC | PackerCompile ]]
   end
 })
+
+vim.api.nvim_create_autocmd('BufWinLeave', {
+  pattern = '*',
+  callback = function()
+    if vim.fn.expand('<afile>'):match('NvimTree') then
+      require("bufferline.state").set_offset(0)
+    end
+  end
+})
