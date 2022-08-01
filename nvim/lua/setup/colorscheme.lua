08b6356a65ecbda35b6576d1b9258e92a3f38f80
Benji Nguyen <benjamin.van.nguyen@gmail.com>
2022-08-01 01:00:24 -0700

    opacity

--- nvim/lua/setup/colorscheme.lua
+++ nvim/lua/setup/colorscheme.lua
@@ -8,6 +8,9 @@ vim.g.tokyonight_style = style.night
 vim.g.tokyonight_italic_comments = false
 vim.g.tokyonight_italic_keywords = false
 vim.g.tokyonight_dark_sidebar = false
+vim.g.tokyonight_transparent = true
+vim.g.tokyonight_transparent_sidebar = true
+vim.g.tokyonight_dark_float = true
 
 require("lualine").setup {
   options = {
