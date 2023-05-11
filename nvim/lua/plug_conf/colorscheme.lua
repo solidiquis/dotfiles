d969778617186110fefed8952a0f11d3d855ecb7
Benjamin Nguyen <benjamin.van.nguyen@gmail.com>
2023-05-10 18:37:38 -0700

    color tweaks

--- nvim/lua/plug_conf/colorscheme.lua
+++ nvim/lua/plug_conf/colorscheme.lua
@@ -2,6 +2,9 @@ local lualine = require("lualine")
 --local colorscheme = require("tokyonight")
 
 vim.g.gruvbox_material_background = "hard"
+vim.g.gruvbox_material_foreground = "original"
+vim.g.gruvbox_material_transparent_background = 1
+
 
 lualine.setup {
   options = {
