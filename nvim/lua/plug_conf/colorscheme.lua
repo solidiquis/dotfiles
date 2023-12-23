b2283efb3f918a7576f2d567a5867ddc0d13336a
Benjamin Nguyen <benjamin.van.nguyen@gmail.com>
2023-12-23 10:53:33 -0800

    no italics/bold

--- nvim/lua/plug_conf/colorscheme.lua
+++ nvim/lua/plug_conf/colorscheme.lua
@@ -1,12 +1,13 @@
 vim.o.background = "dark" -- or "light" for light mode
 vim.o.background = "dark" -- or "light" for light mode
-vim.cmd("let g:gruvbox_material_background= 'hard'")
-vim.cmd("let g:gruvbox_material_transparent_background=2")
-vim.cmd("let g:gruvbox_material_diagnostic_line_highlight=1")
-vim.cmd("let g:gruvbox_material_diagnostic_virtual_text='colored'")
-vim.cmd("let g:gruvbox_material_enable_bold=1")
-vim.cmd("let g:gruvbox_material_enable_italic=1")
-vim.cmd([[colorscheme gruvbox-material]]) -- Set color scheme
+vim.cmd([[
+  let g:gruvbox_material_background= 'hard'
+  let g:gruvbox_material_transparent_background=2
+  let g:gruvbox_material_diagnostic_line_highlight=1
+  let g:gruvbox_material_diagnostic_virtual_text='colored'
+  let g:gruvbox_material_enable_bold=0
+  let g:gruvbox_material_enable_italic=0
+]])
 vim.api.nvim_set_hl(0, "FloatBorder", { link = "Normal" })
 vim.api.nvim_set_hl(0, "LspInfoBorder", { link = "Normal" })
 vim.api.nvim_set_hl(0, "NormalFloat", { link = "Normal" })
