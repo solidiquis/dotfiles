da149197f96d0946a51c08c148657df5cf31a40c
solidiquis <benjamin.van.nguyen@gmail.com>
2024-02-26 21:49:18 -0800

    line height

--- wezterm/font.lua
+++ wezterm/font.lua
@@ -3,7 +3,7 @@ local wezterm = require("wezterm")
 local M = {}
 
 function M.update_config(config)
-  config.line_height = 1.2
+  config.line_height = 1.3
   config.cell_width = 0.95
   config.font = wezterm.font_with_fallback {
     { family = "SauceCodePro NF", weight = "Medium" },
