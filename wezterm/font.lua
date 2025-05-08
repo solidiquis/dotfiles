f9d3c0aa8a99ef063d9d96746dac4f9ac034387f
Benjamin Nguyen <benjamin.van.nguyen@gmail.com>
2025-05-07 22:17:10 -0700

    commit mono

--- wezterm/font.lua
+++ wezterm/font.lua
@@ -3,9 +3,11 @@ local wezterm = require("wezterm")
 local M = {}
 
 function M.update_config(config)
-  config.line_height = 1.5
+  config.line_height = 1.7
   config.cell_width = 1.0
   config.font = wezterm.font_with_fallback {
+    { family = "CommitMono Nerd Font Mono", weight = "Bold", italic = false },
+    { family = "SauceCodePro Nerd Font Mono", weight = "Bold", italic = false },
     { family = "RobotoMono Nerd Font Mono", weight = "Bold", italic = false },
     { family = "JetBrains Mono", weight = "Bold", italic = false },
   }
