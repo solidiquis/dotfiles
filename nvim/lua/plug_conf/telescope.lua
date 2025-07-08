cb75e89a1137cb7502f0489ac1c485588042ca0d
Benjamin Nguyen <benjamin.van.nguyen@gmail.com>
2025-07-08 10:40:57 -0700

    telescope multifile select

--- nvim/lua/plug_conf/telescope.lua
+++ nvim/lua/plug_conf/telescope.lua
@@ -3,8 +3,28 @@ local telescope = require("telescope")
 local builtin = require("telescope.builtin")
 local themes = require("telescope.themes")
 
+local select_one_or_multi = function(prompt_bufnr)
+  local picker = require('telescope.actions.state').get_current_picker(prompt_bufnr)
+  local multi = picker:get_multi_selection()
+  if not vim.tbl_isempty(multi) then
+    require('telescope.actions').close(prompt_bufnr)
+    for _, j in pairs(multi) do
+      if j.path ~= nil then
+        vim.cmd(string.format('%s %s', 'edit', j.path))
+      end
+    end
+  else
+    require('telescope.actions').select_default(prompt_bufnr)
+  end
+end
+
 telescope.setup({
   defaults = {
+    mappings = {
+      i = {
+        ['<CR>'] = select_one_or_multi,
+      },
+    },
     layout_strategy = "vertical",
     layout_config = { preview_cutoff = 0 },
     file_ignore_patterns = {
