9a65b7a3284115bb884cbbfd5ee54360eab84ef8
Benjamin Nguyen <benjamin.van.nguyen@gmail.com>
2025-07-14 10:00:57 -0700

    hook to prompt confirmation for destructive commands

--- zsh/hooks.zsh
+++ zsh/hooks.zsh
@@ -3,3 +3,22 @@ precmd() {
     ibeam_cursor
   fi 
 }
+
+preexec() {
+  local orig_ifs="$IFS"
+  local destructive_cmds=("git checkout ." "git clean -fd" "rm -rf")
+  IFS=$'\n'
+
+  for cmd in "${destructive_cmds[@]}"; do
+    if [[ "$1" != "$cmd" ]]; then
+      continue
+    fi
+    printf "Type 'confirm' to execute '$1': "
+    read
+    if [[ "$REPLY" != "confirm" ]]; then
+      echo "Aborting command"
+      exec zsh
+    fi
+  done
+  IFS="$orig_ifs"
+}
